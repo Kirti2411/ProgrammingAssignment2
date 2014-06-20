327d4bfc5b99b880b859ef0e2a6329004d1252bd
Kirti2411 <mail2kirti11@gmail.com>
2014-06-20 12:15:02 +0800

    Update cachematrix.R
    Adding comments to the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+# The following functions are used for Matrix inversion and the below function are used for caching the result so that the computaions
+# are not done repeatedly if already done.
+#The assumptions are that the input matrix is always square invertible matrix
+
+# The makeCacheMatrix creates a storage object for the inverse of matrix.The method also has get and set functions for setting the 
+# and retrieval of values from the objects.
+
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -11,6 +18,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
+
+# The cacheSolve function creates the inverse of a square matrix.
+# The solve function in R is used for calculating the inverse but the only function is only triggered if the inverse was not calculated previously
 
 cacheSolve <- function(x=matrix(), ...) {
   m<-x$getmatrix()
